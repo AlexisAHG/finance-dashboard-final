ada8998349d19acde6df6d5fd49f6aa5676e795b
Alexis Hanna <alexis.hanna.ah@gmail.com>
2026-01-08 16:19:05 +0100

    quanta: add streamlit page for single-asset backtest

---
+++ pages/1_SingleAsset.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Single Asset", layout="wide")
+st.title("Single-asset backtest (Quant A)")
+
+st.write("BTC/USD momentum / basic backtest. WIP.")
+
+# TODO: call quant-a functions here
+# ex: from src.analytics.xxx import run_backtest
+# result = run_backtest(...)
+# st.line_chart(result["equity_curve"])
